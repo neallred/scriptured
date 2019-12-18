666cc5e32f71825e361084bdbfec15aed0e4a397
neallred <neallred@gmail.com>
2019-12-18 17:45:56 -0600

    Pay cost of building scripture data structures on first search instead of on every search.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,9 +4,13 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+#[macro_use]
+extern crate lazy_static;
+
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::error::Error;
 
 
@@ -25,25 +29,27 @@ macro_rules! log {
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-// TODO: Figure out how to get a static struct in the desired format at compile time.
-// This does not work:
-// static BOM_2: &'static str = serde_json::to_string(serde_json::from_str(&include_str!("../client/node_modules/@bencrowder/scriptures-json/book-of-mormon.json"))?)?;
-
-// TODO: Figure out how to make the compiler run an npm install if the json files do not exist at
-// compile time
-
 //    let npm = Command::new(NPM)
 //        .arg("install")
 //        .status();
 //
 //    println!("npm {:?}", npm);
 
-static BOOK_OF_MORMON: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/book-of-mormon.json");
-static OLD_TESTAMENT: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/old-testament.json");
-static NEW_TESTAMENT: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/new-testament.json");
-static PEARL_OF_GREAT_PRICE: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/pearl-of-great-price.json");
-static DOCTRINE_AND_COVENANTS: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/doctrine-and-covenants.json");
+static STR_BOOK_OF_MORMON: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/book-of-mormon.json");
+static STR_OLD_TESTAMENT: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/old-testament.json");
+static STR_NEW_TESTAMENT: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/new-testament.json");
+static STR_PEARL_OF_GREAT_PRICE: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/pearl-of-great-price.json");
+static STR_DOCTRINE_AND_COVENANTS: &'static str = include_str!("../../node_modules/@bencrowder/scriptures-json/doctrine-and-covenants.json");
+
+// TODO: Figure out to do this one, at compile time.
+lazy_static! {
+    static ref BOOK_OF_MORMON: BookOfMormon = serde_json::from_str(&STR_BOOK_OF_MORMON).unwrap();
+    static ref OLD_TESTAMENT: OldTestament = serde_json::from_str(&STR_OLD_TESTAMENT).unwrap();
+    static ref NEW_TESTAMENT: NewTestament = serde_json::from_str(&STR_NEW_TESTAMENT).unwrap();
+    static ref PEARL_OF_GREAT_PRICE: PearlOfGreatPrice = serde_json::from_str(&STR_PEARL_OF_GREAT_PRICE).unwrap();
+    static ref DOCTRINE_AND_COVENANTS: DoctrineAndCovenants = serde_json::from_str(&STR_DOCTRINE_AND_COVENANTS).unwrap();
+}
+
 
 #[cfg(windows)]
 pub const NPM: &'static str = "npm.cmd";
@@ -218,81 +224,6 @@ pub struct PearlOfGreatPrice {
     version: u64,
 }
 
-pub fn get_bom() -> Result<BookOfMormon, Box<dyn Error>> {
-    Ok(serde_json::from_str(&BOOK_OF_MORMON)?)
-}
-
-pub fn get_ot() -> Result<OldTestament, Box<dyn Error>> {
-    Ok(serde_json::from_str(&OLD_TESTAMENT)?)
-}
-
-pub fn get_nt() -> Result<NewTestament, Box<dyn Error>> {
-    Ok(serde_json::from_str(&NEW_TESTAMENT)?)
-}
-
-pub fn get_pogp() -> Result<PearlOfGreatPrice, Box<dyn Error>> {
-    Ok(serde_json::from_str(&PEARL_OF_GREAT_PRICE)?)
-}
-
-pub fn get_dc() -> Result<DoctrineAndCovenants, Box<dyn Error>> {
-    Ok(serde_json::from_str(&DOCTRINE_AND_COVENANTS)?)
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Scriptures {
-    ot: OldTestament,
-    nt: NewTestament,
-    bom: BookOfMormon,
-    dc: DoctrineAndCovenants,
-    pogp: PearlOfGreatPrice,
-}
-
-pub fn get_scriptures () -> Result<Scriptures, Box<dyn Error>> {
-    Ok(
-        Scriptures {
-            ot: get_ot()?,
-            nt: get_nt()?,
-            bom: get_bom()?,
-            dc: get_dc()?,
-            pogp: get_pogp()?,
-        }
-    )
-}
-
-// Once struct generation happens at compile time, this won't be needed
-#[wasm_bindgen]
-pub fn check_parsing() {
-    let bom_result = get_bom();
-    match bom_result {
-        Ok(_) => log!("got Book of Mormon"),
-        Err(e) => log!("{:?}", e),
-    }
-
-    let ot_result = get_ot();
-    match ot_result {
-        Ok(_) => log!("got Old Testament"),
-        Err(e) => log!("{:?}", e),
-    }
-
-    let nt_result = get_nt();
-    match nt_result {
-        Ok(_) => log!("got New Testatment"),
-        Err(e) => log!("{:?}", e),
-    }
-
-    let pogp_result = get_pogp();
-    match pogp_result {
-        Ok(_) => log!("got Pearl of Great Price"),
-        Err(e) => log!("{:?}", e),
-    }
-
-    let dc_result = get_dc();
-    match dc_result {
-        Ok(_) => log!("got Doctrine and Covenants"),
-        Err(e) => log!("{:?}", e),
-    }
-}
-
 fn format_verse(v: &Verse) -> String {
     format!("{}: {}", &v.reference, &v.text)
 }
@@ -314,64 +245,57 @@ pub fn full_match_search(search_term_raw: String, search_preferences_js: JsValue
         Box::new(case_insensitive_match)
     };
 
-    let final_result = match get_scriptures() {
-        Ok(Scriptures {ot, nt, bom, dc, pogp}) => {
-            let mut results: Vec<String> = vec![];
+    let mut results: Vec<String> = vec![];
 
-            if search_preferences.included_sources.ot {
-                let mut ot_results: Vec<String> = ot.books.iter()
-                    .filter(|book| search_preferences.included_books.ot.contains(&book.book))
-                    .flat_map(|book| &book.chapters)
-                    .flat_map(|chapter| &chapter.verses)
-                    .filter(&verse_search)
-                    .map(format_verse).collect();
+    if search_preferences.included_sources.ot {
+        let mut ot_results: Vec<String> = (&*OLD_TESTAMENT).books.iter()
+            .filter(|book| search_preferences.included_books.ot.contains(&book.book))
+            .flat_map(|book| &book.chapters)
+            .flat_map(|chapter| &chapter.verses)
+            .filter(&verse_search)
+            .map(format_verse).collect();
 
-                results.append(&mut ot_results);
-            }
+        results.append(&mut ot_results);
+    }
 
-            if search_preferences.included_sources.nt {
-                let mut nt_results: Vec<String> = nt.books.iter()
-                    .filter(|book| search_preferences.included_books.nt.contains(&book.book))
-                    .flat_map(|book| &book.chapters)
-                    .flat_map(|chapter| &chapter.verses)
-                    .filter(&verse_search)
-                    .map(format_verse).collect();
-                results.append(&mut nt_results);
-            }
+    if search_preferences.included_sources.nt {
+        let mut nt_results: Vec<String> = (&*NEW_TESTAMENT).books.iter()
+            .filter(|book| search_preferences.included_books.nt.contains(&book.book))
+            .flat_map(|book| &book.chapters)
+            .flat_map(|chapter| &chapter.verses)
+            .filter(&verse_search)
+            .map(format_verse).collect();
+        results.append(&mut nt_results);
+    }
 
-            if search_preferences.included_sources.bom {
-                let mut bom_results: Vec<String> = bom.books.iter()
-                    .filter(|book| search_preferences.included_books.bom.contains(&book.book))
-                    .flat_map(|book| &book.chapters)
-                    .flat_map(|chapter| &chapter.verses)
-                    .filter(&verse_search)
-                    .map(format_verse).collect();
-                results.append(&mut bom_results);
-            }
+    if search_preferences.included_sources.bom {
+        let mut bom_results: Vec<String> = (&*BOOK_OF_MORMON).books.iter()
+            .filter(|book| search_preferences.included_books.bom.contains(&book.book))
+            .flat_map(|book| &book.chapters)
+            .flat_map(|chapter| &chapter.verses)
+            .filter(&verse_search)
+            .map(format_verse).collect();
+        results.append(&mut bom_results);
+    }
 
-            if search_preferences.included_sources.dc {
-                let mut dc_results: Vec<String> = dc.sections.iter()
-                    .filter(|section| inclusive_contains(section.section, search_preferences.included_books.dc))
-                    .flat_map(|section| &section.verses)
-                    .filter(&verse_search)
-                    .map(format_verse).collect();
-                results.append(&mut dc_results);
-            }
+    if search_preferences.included_sources.dc {
+        let mut dc_results: Vec<String> = (&*DOCTRINE_AND_COVENANTS).sections.iter()
+            .filter(|section| inclusive_contains(section.section, search_preferences.included_books.dc))
+            .flat_map(|section| &section.verses)
+            .filter(&verse_search)
+            .map(format_verse).collect();
+        results.append(&mut dc_results);
+    }
 
-            if search_preferences.included_sources.pogp {
-                let mut pogp_results: Vec<String> = pogp.books.iter()
-                    .filter(|book| search_preferences.included_books.pogp.contains(&book.book))
-                    .flat_map(|book| &book.chapters)
-                    .flat_map(|chapter| &chapter.verses)
-                    .filter(&verse_search)
-                    .map(format_verse).collect();
-                results.append(&mut pogp_results);
-            }
+    if search_preferences.included_sources.pogp {
+        let mut pogp_results: Vec<String> = (&*PEARL_OF_GREAT_PRICE).books.iter()
+            .filter(|book| search_preferences.included_books.pogp.contains(&book.book))
+            .flat_map(|book| &book.chapters)
+            .flat_map(|chapter| &chapter.verses)
+            .filter(&verse_search)
+            .map(format_verse).collect();
+        results.append(&mut pogp_results);
+    }
 
-            results
-        },
-        Err(err) => vec![format!("Error getting scriptures {:?}", err)],
-    };
-
-    JsValue::from_serde(&final_result).unwrap()
+    JsValue::from_serde(&results).unwrap()
 }
