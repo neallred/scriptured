a29625b3f47cb4a3609f8821e201f0d37591e2d9
Nathaniel Allred <neallred@gmail.com>
2020-01-02 21:40:32 -0600

    Index (#2)
    * WIP building search index
    
    * Create indices for word usage in verses and verse paths
    
    * working index for all chapters and sections. SLOW building it on the client.
    
    * Build index on compile. Parse index on client. WIP use index in searches
    
    * first working index search
    
    * add explanations for some build failures
    
    * better bootstrapping
    
    * better error messaging for missing files
    
    * rustfmt
    
    * use index for searches
    
    * tweak bootstrap

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,41 +1,49 @@
 mod utils;
 
+extern crate rust_stemmers;
+extern crate scripture_types;
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
-extern crate scripture_types;
+use regex::Regex;
+use rust_stemmers::{Algorithm, Stemmer};
 
 #[macro_use]
 extern crate lazy_static;
 
-use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
 use flate2::read::GzDecoder;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::io::prelude::*;
-// use std::collections::HashSet;
+use wasm_bindgen::prelude::*;
+// use std::collections::HashMap;
 // use std::error::Error;
+use scripture_types::{
+    BookOfMormon, DoctrineAndCovenants, NewTestament, OldTestament, PathsIndex, PearlOfGreatPrice,
+    VersePath, WordsIndex,
+};
 
-// extern crate web_sys;
-// use web_sys::console;
-// macro_rules! log {
-//     ( $( $t:tt )* ) => {
-//         console::log_1(&format!( $( $t )* ).into());
-//     }
-// }
+extern crate web_sys;
+use web_sys::console;
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SearchPreferences {
-  pub and: bool,
-  #[serde(rename = "caseSensitive")]
-  pub case_sensitive: bool,
-  pub exact: bool,
-  #[serde(rename = "includedSources")]
-  pub included_sources: IncludedSources,
-  #[serde(rename = "includedBooks")]
-  pub included_books: IncludedBooks,
+    pub and: bool,
+    #[serde(rename = "caseSensitive")]
+    pub case_sensitive: bool,
+    pub exact: bool,
+    #[serde(rename = "includedSources")]
+    pub included_sources: IncludedSources,
+    #[serde(rename = "includedBooks")]
+    pub included_books: IncludedBooks,
 }
 
 #[wasm_bindgen]
@@ -62,45 +70,45 @@ pub struct IncludedBooks {
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+static BYTES_OLD_TESTAMENT: &'static [u8] =
+    include_bytes!("../../data-bundler/data/old-testament.json.gz");
+static BYTES_NEW_TESTAMENT: &'static [u8] =
+    include_bytes!("../../data-bundler/data/new-testament.json.gz");
+static BYTES_BOOK_OF_MORMON: &'static [u8] =
+    include_bytes!("../../data-bundler/data/book-of-mormon.json.gz");
+static BYTES_DOCTRINE_AND_COVENANTS: &'static [u8] =
+    include_bytes!("../../data-bundler/data/doctrine-and-covenants.json.gz");
+static BYTES_PEARL_OF_GREAT_PRICE: &'static [u8] =
+    include_bytes!("../../data-bundler/data/pearl-of-great-price.json.gz");
 
-static BYTES_OLD_TESTAMENT: &'static [u8] = include_bytes!("../../data-bundler/data/old-testament.json.gz");
-static BYTES_NEW_TESTAMENT: &'static [u8] = include_bytes!("../../data-bundler/data/new-testament.json.gz");
-static BYTES_BOOK_OF_MORMON: &'static [u8] = include_bytes!("../../data-bundler/data/book-of-mormon.json.gz");
-static BYTES_DOCTRINE_AND_COVENANTS: &'static [u8] = include_bytes!("../../data-bundler/data/doctrine-and-covenants.json.gz");
-static BYTES_PEARL_OF_GREAT_PRICE: &'static [u8] = include_bytes!("../../data-bundler/data/pearl-of-great-price.json.gz");
+static BYTES_WORDS_INDEX: &'static [u8] =
+    include_bytes!("../../data-bundler/data/words-index.json.gz");
+static BYTES_PATHS_INDEX: &'static [u8] =
+    include_bytes!("../../data-bundler/data/paths-index.json.gz");
 
 // TODO: Figure out to do this one, at compile time.
 lazy_static! {
-    static ref BOOK_OF_MORMON: scripture_types::BookOfMormon = parse_gzip(&BYTES_BOOK_OF_MORMON);
-    static ref OLD_TESTAMENT: scripture_types::OldTestament = parse_gzip(&BYTES_OLD_TESTAMENT);
-    static ref NEW_TESTAMENT: scripture_types::NewTestament = parse_gzip(&BYTES_NEW_TESTAMENT);
-    static ref PEARL_OF_GREAT_PRICE: scripture_types::PearlOfGreatPrice = parse_gzip(&BYTES_PEARL_OF_GREAT_PRICE);
-    static ref DOCTRINE_AND_COVENANTS: scripture_types::DoctrineAndCovenants = parse_gzip(&BYTES_DOCTRINE_AND_COVENANTS);
+    static ref BOOK_OF_MORMON: BookOfMormon = parse_gzip(&BYTES_BOOK_OF_MORMON);
+    static ref OLD_TESTAMENT: OldTestament = parse_gzip(&BYTES_OLD_TESTAMENT);
+    static ref NEW_TESTAMENT: NewTestament = parse_gzip(&BYTES_NEW_TESTAMENT);
+    static ref PEARL_OF_GREAT_PRICE: PearlOfGreatPrice = parse_gzip(&BYTES_PEARL_OF_GREAT_PRICE);
+    static ref DOCTRINE_AND_COVENANTS: DoctrineAndCovenants =
+        parse_gzip(&BYTES_DOCTRINE_AND_COVENANTS);
+    static ref WORDS_INDEX: WordsIndex = parse_gzip(&BYTES_WORDS_INDEX);
+    static ref PATHS_INDEX: PathsIndex = parse_gzip(&BYTES_PATHS_INDEX);
+    static ref STEMMER: rust_stemmers::Stemmer = Stemmer::create(Algorithm::English);
+    static ref RE_VERSE_CHARS: Regex = Regex::new(r"[^A-Za-z0-9\sæ\-]").unwrap();
 }
-
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-// enum AndOr {
-//     And = 1,
-//     Or = 0,
-// }
 
 fn format_verse(v: &scripture_types::Verse) -> String {
     format!("{}: {}", &v.reference, &v.text)
 }
 
-fn inclusive_contains(x: u64, bounds: (u64, u64)) -> bool {
-    x >= bounds.0 && x <= bounds.1
-}
+// fn inclusive_contains(x: u64, bounds: (u64, u64)) -> bool {
+//     x >= bounds.0 && x <= bounds.1
+// }
 
-pub fn parse_gzip<T: serde::de::DeserializeOwned + serde::ser::Serialize>(
-    gzipped: &[u8]
-) -> T {
-
+pub fn parse_gzip<T: serde::de::DeserializeOwned + serde::ser::Serialize>(gzipped: &[u8]) -> T {
     let mut d = GzDecoder::new(gzipped);
     let mut s = String::new();
     d.read_to_string(&mut s).unwrap();
@@ -133,75 +141,148 @@ pub fn bootstrap_searcher() {
         },
     };
     full_match_search(
-        String::from("BOOSTRAP SEARCHER BOOSTRAP SEARCHER BOOSTRAP SEARCHER"),
+        String::from("BOOSTRAP SCRIPTURED SEARCHER"),
         JsValue::from_serde(&empty_preferences).unwrap(),
     );
+    log!("words: {:?}", WORDS_INDEX.len());
+    log!("paths: {:?}", PATHS_INDEX.len());
+}
+
+fn make_splittable(text: &String) -> String {
+    let with_substitutions = text
+        .replace("–", " ")
+        .replace("—", " ")
+        .replace("—", " ")
+        .replace("'s", "")
+        .to_lowercase();
+    let splittable = RE_VERSE_CHARS.replace_all(&with_substitutions, "");
+    splittable.to_string()
+}
+
+pub fn resolve_verse_path(
+    path: &VersePath,
+    _preferences: &SearchPreferences,
+) -> &'static scripture_types::Verse {
+    match path {
+        VersePath::PathOT(b, c, v) => &(&*OLD_TESTAMENT).books[*b].chapters[*c].verses[*v],
+        VersePath::PathNT(b, c, v) => &(&*NEW_TESTAMENT).books[*b].chapters[*c].verses[*v],
+        VersePath::PathBoM(b, c, v) => &(&*BOOK_OF_MORMON).books[*b].chapters[*c].verses[*v],
+        VersePath::PathDC(s, v) => &(&*DOCTRINE_AND_COVENANTS).sections[*s].verses[*v],
+        VersePath::PathPOGP(b, c, v) => &(&*PEARL_OF_GREAT_PRICE).books[*b].chapters[*c].verses[*v],
+    }
 }
 
 #[wasm_bindgen]
 pub fn full_match_search(search_term_raw: String, search_preferences_js: JsValue) -> JsValue {
     let search_preferences: SearchPreferences = search_preferences_js.into_serde().unwrap();
     let search_term = &search_term_raw.to_lowercase();
-    let case_sensitive_match = |verse: &&scripture_types::Verse| verse.text.contains(&search_term_raw);
-    let case_insensitive_match = |verse: &&scripture_types::Verse| verse.text.to_lowercase().contains(search_term);
+    // let case_sensitive_match =
+    //     |verse: &&scripture_types::Verse| verse.text.contains(&search_term_raw);
+    // let case_insensitive_match =
+    //     |verse: &&scripture_types::Verse| verse.text.to_lowercase().contains(search_term);
 
-    let verse_search: Box<dyn Fn(&&scripture_types::Verse) -> bool> = if search_preferences.case_sensitive {
-        Box::new(case_sensitive_match)
-    } else {
-        Box::new(case_insensitive_match)
-    };
+    // let verse_search: Box<dyn Fn(&&scripture_types::Verse) -> bool> =
+    //     if search_preferences.case_sensitive {
+    //         Box::new(case_sensitive_match)
+    //     } else {
+    //         Box::new(case_insensitive_match)
+    //     };
 
-    let mut results: Vec<String> = vec![];
+    let all_verses: HashSet<u32> = (1..(&*PATHS_INDEX).len() as u32).collect();
+    // TODO: Use this for "or" searches
+    // let no_verses: HashSet<u32> = HashSet::new();
+    let stemmed_search = STEMMER.stem(search_term);
 
-    if search_preferences.included_sources.ot {
-        let mut ot_results: Vec<String> = (&*OLD_TESTAMENT).books.iter()
-            .filter(|book| search_preferences.included_books.ot.contains(&book.book))
-            .flat_map(|book| &book.chapters)
-            .flat_map(|chapter| &chapter.verses)
-            .filter(&verse_search)
-            .map(format_verse).collect();
+    let index_results = make_splittable(&stemmed_search.to_string())
+        .split_whitespace()
+        .fold(all_verses, |acc, word| {
+            let index_results = match (&*WORDS_INDEX).get(word) {
+                Some(x) => {
+                    let in_both = acc.intersection(x).map(|&x| x).collect();
+                    in_both
+                }
+                None => {
+                    HashSet::new()
+                }
+            };
+            index_results
+        });
+    let verses: Vec<String> = index_results
+        .iter()
+        .map(|x| (&*PATHS_INDEX).get(x).unwrap())
+        .map(|x| resolve_verse_path(x, &search_preferences))
+        .map(format_verse)
+        .collect();
+    JsValue::from_serde(&verses).unwrap()
 
-        results.append(&mut ot_results);
-    }
+    // TODO: Use this code to further filter potential matches
+    // let mut results: Vec<String> = vec![];
+    // if search_preferences.included_sources.ot {
+    //     let mut ot_results: Vec<String> = (&*OLD_TESTAMENT)
+    //         .books
+    //         .iter()
+    //         .filter(|book| search_preferences.included_books.ot.contains(&book.book))
+    //         .flat_map(|book| &book.chapters)
+    //         .flat_map(|chapter| &chapter.verses)
+    //         .filter(&verse_search)
+    //         .map(format_verse)
+    //         .collect();
 
-    if search_preferences.included_sources.nt {
-        let mut nt_results: Vec<String> = (&*NEW_TESTAMENT).books.iter()
-            .filter(|book| search_preferences.included_books.nt.contains(&book.book))
-            .flat_map(|book| &book.chapters)
-            .flat_map(|chapter| &chapter.verses)
-            .filter(&verse_search)
-            .map(format_verse).collect();
-        results.append(&mut nt_results);
-    }
+    //     results.append(&mut ot_results);
+    // }
 
-    if search_preferences.included_sources.bom {
-        let mut bom_results: Vec<String> = (&*BOOK_OF_MORMON).books.iter()
-            .filter(|book| search_preferences.included_books.bom.contains(&book.book))
-            .flat_map(|book| &book.chapters)
-            .flat_map(|chapter| &chapter.verses)
-            .filter(&verse_search)
-            .map(format_verse).collect();
-        results.append(&mut bom_results);
-    }
+    // if search_preferences.included_sources.nt {
+    //     let mut nt_results: Vec<String> = (&*NEW_TESTAMENT)
+    //         .books
+    //         .iter()
+    //         .filter(|book| search_preferences.included_books.nt.contains(&book.book))
+    //         .flat_map(|book| &book.chapters)
+    //         .flat_map(|chapter| &chapter.verses)
+    //         .filter(&verse_search)
+    //         .map(format_verse)
+    //         .collect();
+    //     results.append(&mut nt_results);
+    // }
 
-    if search_preferences.included_sources.dc {
-        let mut dc_results: Vec<String> = (&*DOCTRINE_AND_COVENANTS).sections.iter()
-            .filter(|section| inclusive_contains(section.section, search_preferences.included_books.dc))
-            .flat_map(|section| &section.verses)
-            .filter(&verse_search)
-            .map(format_verse).collect();
-        results.append(&mut dc_results);
-    }
+    // if search_preferences.included_sources.bom {
+    //     let mut bom_results: Vec<String> = (&*BOOK_OF_MORMON)
+    //         .books
+    //         .iter()
+    //         .filter(|book| search_preferences.included_books.bom.contains(&book.book))
+    //         .flat_map(|book| &book.chapters)
+    //         .flat_map(|chapter| &chapter.verses)
+    //         .filter(&verse_search)
+    //         .map(format_verse)
+    //         .collect();
+    //     results.append(&mut bom_results);
+    // }
 
-    if search_preferences.included_sources.pogp {
-        let mut pogp_results: Vec<String> = (&*PEARL_OF_GREAT_PRICE).books.iter()
-            .filter(|book| search_preferences.included_books.pogp.contains(&book.book))
-            .flat_map(|book| &book.chapters)
-            .flat_map(|chapter| &chapter.verses)
-            .filter(&verse_search)
-            .map(format_verse).collect();
-        results.append(&mut pogp_results);
-    }
+    // if search_preferences.included_sources.dc {
+    //     let mut dc_results: Vec<String> = (&*DOCTRINE_AND_COVENANTS)
+    //         .sections
+    //         .iter()
+    //         .filter(|section| {
+    //             inclusive_contains(section.section, search_preferences.included_books.dc)
+    //         })
+    //         .flat_map(|section| &section.verses)
+    //         .filter(&verse_search)
+    //         .map(format_verse)
+    //         .collect();
+    //     results.append(&mut dc_results);
+    // }
 
-    JsValue::from_serde(&results).unwrap()
+    // if search_preferences.included_sources.pogp {
+    //     let mut pogp_results: Vec<String> = (&*PEARL_OF_GREAT_PRICE)
+    //         .books
+    //         .iter()
+    //         .filter(|book| search_preferences.included_books.pogp.contains(&book.book))
+    //         .flat_map(|book| &book.chapters)
+    //         .flat_map(|chapter| &chapter.verses)
+    //         .filter(&verse_search)
+    //         .map(format_verse)
+    //         .collect();
+    //     results.append(&mut pogp_results);
+    // }
+
+    // JsValue::from_serde(&results).unwrap()
 }
--- data-bundler/src/main.rs
+++ data-bundler/src/main.rs
@@ -1,3 +1,4 @@
+extern crate rust_stemmers;
 extern crate scripture_types;
 use std::fs::File;
 use std::io::BufReader;
@@ -5,15 +6,230 @@ use std::io::BufWriter;
 use std::io::Read;
 use std::path::Path;
 
-use std::io::prelude::*;
-use flate2::Compression;
 use flate2::write::GzEncoder;
+use flate2::Compression;
+use regex::Regex;
+use rust_stemmers::{Algorithm, Stemmer};
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io::prelude::*;
 
 #[cfg(windows)]
 pub const NPM: &'static str = "npm.cmd";
 
 #[cfg(not(windows))]
 pub const NPM: &'static str = "npm";
+
+pub enum HasBooks<'a> {
+    OT(&'a scripture_types::OldTestament),
+    NT(&'a scripture_types::NewTestament),
+    BOM(&'a scripture_types::BookOfMormon),
+    POGP(&'a scripture_types::PearlOfGreatPrice),
+}
+
+fn prepare_book_paths<'a>(coll: HasBooks<'a>) -> Vec<(usize, usize, &'a scripture_types::Verse)> {
+    let (books, title) = match coll {
+        HasBooks::OT(x) => (&x.books, &x.title),
+        HasBooks::NT(x) => (&x.books, &x.title),
+        HasBooks::BOM(x) => (&x.books, &x.title),
+        HasBooks::POGP(x) => (&x.books, &x.title),
+    };
+    println!("    {}", title);
+    let with_books: Vec<(usize, &scripture_types::Chapter)> = books
+        .iter()
+        .enumerate()
+        .flat_map(|(book_num, book)| {
+            let with_books: Vec<(usize, &scripture_types::Chapter)> =
+                book.chapters.iter().map(|cs| (book_num, cs)).collect();
+
+            with_books
+        })
+        .collect();
+
+    let with_chapters = with_books
+        .iter()
+        .flat_map(|(book_num, chapter)| {
+            let with_verses: Vec<(usize, usize, &scripture_types::Verse)> = chapter
+                .verses
+                .iter()
+                .map(|v| (*book_num, chapter.chapter as usize - 1, v))
+                .collect();
+
+            with_verses
+        })
+        .collect();
+
+    with_chapters
+}
+
+fn build_index(
+    ot: scripture_types::OldTestament,
+    nt: scripture_types::NewTestament,
+    bom: scripture_types::BookOfMormon,
+    dc: scripture_types::DoctrineAndCovenants,
+    pogp: scripture_types::PearlOfGreatPrice,
+) -> (scripture_types::WordsIndex, scripture_types::PathsIndex) {
+    let mut scripture_id: u32 = 0;
+
+    let re_verse_chars: Regex = Regex::new(r"[^A-Za-z0-9\sæ\-]").unwrap();
+    let en_stemmer = Stemmer::create(Algorithm::English);
+
+    let make_splittable = |text: &String| -> String {
+        let with_substitutions = text
+            .replace("–", " ")
+            .replace("—", " ")
+            .replace("—", " ")
+            .replace("'s", "")
+            .to_lowercase();
+        let splittable = re_verse_chars.replace_all(&with_substitutions, "");
+        splittable.to_string()
+    };
+
+    let indices: (scripture_types::WordsIndex, scripture_types::PathsIndex) =
+        (HashMap::new(), HashMap::new());
+
+    let count_word_usage = |mut words_index: scripture_types::WordsIndex, word: &str, id| {
+        let stemmed = en_stemmer.stem(word);
+
+        words_index.insert(
+            stemmed.to_string(),
+            match words_index.get(&stemmed.to_string()) {
+                Some(x) => {
+                    let mut verses_using_word = x.clone();
+                    verses_using_word.insert(id);
+                    verses_using_word
+                }
+                None => {
+                    let mut verses_using_word = HashSet::new();
+                    verses_using_word.insert(id);
+                    verses_using_word
+                }
+            },
+        );
+        words_index
+    };
+
+    let count_verse = |verse_text: &String, words_index: scripture_types::WordsIndex, id| {
+        let index_with_verse_added = make_splittable(verse_text)
+            .split_whitespace()
+            .fold(words_index, |acc, word| count_word_usage(acc, word, id));
+        index_with_verse_added
+    };
+
+    // Old Testament
+    let indices = prepare_book_paths(HasBooks::OT(&ot)).iter().fold(
+        indices,
+        |(words_index, mut path_index), (book_num, chapter_num, verse)| {
+            scripture_id += 1;
+            path_index.insert(
+                scripture_id,
+                scripture_types::VersePath::PathOT(
+                    *book_num,
+                    *chapter_num,
+                    verse.verse as usize - 1,
+                ),
+            );
+            (
+                count_verse(&verse.text, words_index, scripture_id),
+                path_index,
+            )
+        },
+    );
+
+    // New Testament
+    let indices = prepare_book_paths(HasBooks::NT(&nt)).iter().fold(
+        indices,
+        |(words_index, mut path_index), (book_num, chapter_num, verse)| {
+            scripture_id += 1;
+            path_index.insert(
+                scripture_id,
+                scripture_types::VersePath::PathNT(
+                    *book_num,
+                    *chapter_num,
+                    verse.verse as usize - 1,
+                ),
+            );
+            (
+                count_verse(&verse.text, words_index, scripture_id),
+                path_index,
+            )
+        },
+    );
+
+    // Book of Mormon
+    let indices = prepare_book_paths(HasBooks::BOM(&bom)).iter().fold(
+        indices,
+        |(words_index, mut path_index), (book_num, chapter_num, verse)| {
+            scripture_id += 1;
+            path_index.insert(
+                scripture_id,
+                scripture_types::VersePath::PathBoM(
+                    *book_num,
+                    *chapter_num,
+                    verse.verse as usize - 1,
+                ),
+            );
+            (
+                count_verse(&verse.text, words_index, scripture_id),
+                path_index,
+            )
+        },
+    );
+
+    // Doctrine and Covenants
+    println!("    {}", &dc.title);
+    let with_section_nums: Vec<(usize, &scripture_types::Verse)> = (&dc)
+        .sections
+        .iter()
+        .flat_map(|section| {
+            let with_section_nums: Vec<(usize, &scripture_types::Verse)> = section
+                .verses
+                .iter()
+                .map(|v| (section.section as usize - 1, v))
+                .collect();
+
+            with_section_nums
+        })
+        .collect();
+
+    let indices = with_section_nums.iter().fold(
+        indices,
+        |(words_index, mut path_index), (section_num, verse)| {
+            scripture_id += 1;
+            path_index.insert(
+                scripture_id,
+                scripture_types::VersePath::PathDC(*section_num, verse.verse as usize - 1),
+            );
+            (
+                count_verse(&verse.text, words_index, scripture_id),
+                path_index,
+            )
+        },
+    );
+
+    // Pearl of Great Price
+    let indices = prepare_book_paths(HasBooks::POGP(&pogp)).iter().fold(
+        indices,
+        |(words_index, mut path_index), (book_num, chapter_num, verse)| {
+            scripture_id += 1;
+            path_index.insert(
+                scripture_id,
+                scripture_types::VersePath::PathPOGP(*book_num, *chapter_num, verse.verse as usize),
+            );
+            (
+                count_verse(&verse.text, words_index, scripture_id),
+                path_index,
+            )
+        },
+    );
+
+    indices
+}
+
+// enum AndOr {
+//     And = 1,
+//     Or = 0,
+// }
 
 fn ensure_data_source(test_path: &std::path::PathBuf) {
     if Path::new(&test_path).exists() {
@@ -29,13 +245,12 @@ fn ensure_data_source(test_path: &std::path::PathBuf) {
 }
 
 pub fn read_file(filepath: &str) -> String {
-    let file = File::open(filepath)
-        .expect("could not open file");
+    let file = File::open(filepath).expect("could not open file");
     let mut buffered_reader = BufReader::new(file);
     let mut contents = String::new();
     let _number_of_bytes: usize = match buffered_reader.read_to_string(&mut contents) {
         Ok(number_of_bytes) => number_of_bytes,
-        Err(_err) => 0
+        Err(_err) => 0,
     };
 
     contents
@@ -45,10 +260,10 @@ pub fn copy_minified<T: serde::de::DeserializeOwned + serde::ser::Serialize>(
     src_folder: &std::path::PathBuf,
     dest_folder: &std::path::PathBuf,
     file_name: &str,
-) -> () {
+) -> T {
     println!("    {}", file_name);
     let mut src = src_folder.clone();
-    src.push(file_name); 
+    src.push(file_name);
 
     let unparsed: String = read_file(&src.into_os_string().into_string().unwrap());
     let parsed: T = serde_json::from_str(&unparsed).unwrap();
@@ -58,6 +273,26 @@ pub fn copy_minified<T: serde::de::DeserializeOwned + serde::ser::Serialize>(
 
     let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
     serde_json::to_writer(&mut encoder, &parsed).unwrap();
+
+    let mut f_gzipped = BufWriter::new(File::create(dest).unwrap());
+    let gzipped = encoder.finish().unwrap();
+    f_gzipped.write(&gzipped).unwrap();
+
+    parsed
+}
+
+pub fn write_minified<T: serde::ser::Serialize>(
+    data: &T,
+    dest_folder: &std::path::PathBuf,
+    file_name: &str,
+) -> () {
+    let mut dest = dest_folder.clone();
+    dest.push(format!("{}.{}", file_name, "gz"));
+
+    println!("writing {}", file_name);
+
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+    serde_json::to_writer(&mut encoder, data).unwrap();
 
     let mut f_gzipped = BufWriter::new(File::create(dest).unwrap());
     let gzipped = encoder.finish().unwrap();
@@ -76,7 +311,7 @@ fn main() {
     src_folder.push("scriptures-json");
 
     let mut test_path = src_folder.clone();
-    test_path.push("old-testament.json"); 
+    test_path.push("old-testament.json");
     ensure_data_source(&test_path);
 
     let mut dest_folder = project_root.clone();
@@ -84,40 +319,48 @@ fn main() {
     dest_folder.push("data");
 
     // TODO: is there a way to have a list of concrete values and types,
-    // and interate on each of them?
+    // and iterate on each of them?
     // AFAIK, you can't mix term and type levels like that in Haskell.
     // let sources = vec![
     //     ("old-testament.json", scripture_types::OldTestament),
     // ];
     println!("Minifying:");
-    copy_minified::<scripture_types::OldTestament>(
+    let ot = copy_minified::<scripture_types::OldTestament>(
         &src_folder,
         &dest_folder,
         "old-testament.json",
     );
 
-    copy_minified::<scripture_types::NewTestament>(
+    let nt = copy_minified::<scripture_types::NewTestament>(
         &src_folder,
         &dest_folder,
         "new-testament.json",
     );
 
-    copy_minified::<scripture_types::BookOfMormon>(
+    let bom = copy_minified::<scripture_types::BookOfMormon>(
         &src_folder,
         &dest_folder,
         "book-of-mormon.json",
     );
 
-    copy_minified::<scripture_types::DoctrineAndCovenants>(
+    let dc = copy_minified::<scripture_types::DoctrineAndCovenants>(
         &src_folder,
         &dest_folder,
         "doctrine-and-covenants.json",
     );
 
-    copy_minified::<scripture_types::PearlOfGreatPrice>(
+    let pogp = copy_minified::<scripture_types::PearlOfGreatPrice>(
         &src_folder,
         &dest_folder,
         "pearl-of-great-price.json",
     );
-    println!("done!\n");
+    println!("Minifying done!\n");
+
+    println!("Building indices:");
+    let (words_index, paths_index) = build_index(ot, nt, bom, dc, pogp);
+    println!("Index building done!\n");
+    println!("total word stems: {}", words_index.len());
+    println!("total paths: {}", paths_index.len());
+    write_minified(&words_index, &dest_folder, "words-index.json");
+    write_minified(&paths_index, &dest_folder, "paths-index.json");
 }
